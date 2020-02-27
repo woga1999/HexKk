dd644c15891fdf378212d3ae987fdeffc01a81d6
minseok0818@naver.com <minseok0818@naver.com>
2020-02-27 21:07:26 +0900

    update message

---
+++ c_practice/algorithm_01_sort.c
@@ -0,0 +1,34 @@
+//
+//  algorithm_01_sort.c
+//  study c
+//
+//  Created by 김민석 on 2020/02/25.
+//  Copyright © 2020 김민석. All rights reserved.
+//
+
+#include "algorithm_01_sort.h"
+
+//int array[1001];
+//
+//int main(){
+//    int  number, i, j, min, index, temp;
+//    scanf("%d", &number);
+//    for(i = 0; i< number; i++){
+//        scanf("%d", &array[i]);
+//    }
+//    for(i = 0; i < number; i++){
+//        min = 1001;
+//        for(j = i; j < number; j++){
+//            if(min > array[j]){
+//                min = array[j];
+//                index = j;
+//            }
+//        }
+//        temp = array[i];
+//        array[i] = array[index];
+//        array[index] = temp;
+//    }
+//    for(i = 0; i < number; i++){
+//        printf("%d", array[index]);
+//    }
+//}
