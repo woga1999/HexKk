c3864b6b11e594dd8c364960322233cced97d4aa
seunghunpark1 <parksunghun785803@gmail.com>
2020-03-04 14:06:08 +0900

    file update

---
+++ pks/codeblocks/cdblck/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int ReturnPlusOne(int n)
+{
+    return n + 1;
+}
+
+int main(void)
+{
+    number = 0;
+    printf("%d\n",number);
+
+    number = ReturnPlusOne(number);
+    printf("%d\n",number);
+
+    return 0;
+}
